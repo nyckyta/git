f05d8a6d53934c46c7d4ad13b00df1b4047014e8
Thomas Braun <thomas.braun@virtuell-zuhause.de>
2019-01-31 10:25:01 +0100

    Add test for large files on windows
    Original test by Thomas.
    Add the extra fsck to get diagnostics after the add.
    Verify the pack at earliest opportunity
    Slight confusion as to why index-pack vs verify-pack...
    It's -v (verbose) not --verify
    Specify an output file to index-pack, otherwise it clashes with
    the existing index file.
    Check that the sha1 matches the existing value;-)
    
    Signed-off-by: Thomas Braun <thomas.braun@virtuell-zuhause.de>
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

---
+++ t/t-large-files-on-windows.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+test_description='test large file handling on windows'
+. ./test-lib.sh
+
+test_expect_success SIZE_T_IS_64BIT 'require 64bit size_t' '
+
+	dd if=/dev/zero of=file bs=1M count=4100 &&
+	git config core.compression 0 &&
+	git config core.looseCompression 0 &&
+	git add file &&
+	git verify-pack -s .git/objects/pack/*.pack &&
+	git fsck --verbose --strict --full &&
+	git commit -m msg file &&
+	git log --stat &&
+	git gc &&
+	git fsck --verbose --strict --full &&
+	git index-pack -v -o test.idx .git/objects/pack/*.pack &&
+	git gc &&
+	git fsck
+'
+
+test_done
