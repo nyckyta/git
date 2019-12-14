a239a7db0f88a4213c3c500268ba4ae84342eeb0
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-12-14 23:23:30 +0100

    fsmonitor--daemon: implement the core framework
    This patch adds a scaffolding for an internal Git-aware FSMonitor, in
    preparation for adding platform-specific backends.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/fsmonitor--daemon.c
+++ builtin/fsmonitor--daemon.c
@@ -9,11 +9,150 @@
 #include "builtin.h"
 #include "parse-options.h"
 #include "fsmonitor.h"
+#include "simple-ipc.h"
 
 static const char * const builtin_fsmonitor__daemon_usage[] = {
 	N_("git fsmonitor--daemon [<options>]"),
 	NULL
 };
+
+#ifndef HAVE_FSMONITOR_DAEMON_BACKEND
+#define FSMONITOR_DAEMON_IS_SUPPORTED 0
+
+static int fsmonitor_run_daemon(void)
+{
+	die(_("no native fsmonitor daemon available"));
+}
+#else
+#define FSMONITOR_DAEMON_IS_SUPPORTED 1
+
+struct ipc_data {
+	struct ipc_command_listener data;
+	struct fsmonitor_daemon_state *state;
+};
+
+static int handle_client(struct ipc_command_listener *data, const char *command,
+			 ipc_reply_fn_t reply, void *reply_data)
+{
+	struct fsmonitor_daemon_state *state = ((struct ipc_data *)data)->state;
+	unsigned long version;
+	uintmax_t since;
+	char *p;
+	struct fsmonitor_queue_item *queue;
+	struct strbuf token = STRBUF_INIT;
+
+	version = strtoul(command, &p, 10);
+	if (version != FSMONITOR_VERSION) {
+		error(_("fsmonitor: unhandled version (%lu, command: %s)"),
+		      version, command);
+error:
+		pthread_mutex_lock(&state->queue_update_lock);
+		strbuf_addf(&token, "%"PRIu64"", state->latest_update);
+		pthread_mutex_unlock(&state->queue_update_lock);
+		reply(reply_data, token.buf, token.len + 1);
+		reply(reply_data, "/", 2);
+		strbuf_release(&token);
+		return -1;
+	}
+	while (isspace(*p))
+		p++;
+	since = strtoumax(p, &p, 10);
+	if (!since || since < state->latest_update || *p) {
+		pthread_mutex_unlock(&state->queue_update_lock);
+		error(_("fsmonitor: %s (%" PRIuMAX", command: %s, rest %s)"),
+		      *p ? "extra stuff" : "incorrect/early timestamp",
+		      since, command, p);
+		goto error;
+	}
+
+	pthread_mutex_lock(&state->queue_update_lock);
+	queue = state->first;
+	strbuf_addf(&token, "%"PRIu64"", state->latest_update);
+	pthread_mutex_unlock(&state->queue_update_lock);
+
+	reply(reply_data, token.buf, token.len + 1);
+	while (queue && queue->time >= since) {
+		/* write the path, followed by a NUL */
+		if (reply(reply_data,
+			  queue->path->path, queue->path->len + 1) < 0)
+			break;
+		queue = queue->next;
+	}
+
+	strbuf_release(&token);
+	return 0;
+}
+
+static int paths_cmp(const void *data, const struct hashmap_entry *he1,
+		     const struct hashmap_entry *he2, const void *keydata)
+{
+	const struct fsmonitor_path *a =
+		container_of(he1, const struct fsmonitor_path, entry);
+	const struct fsmonitor_path *b =
+		container_of(he2, const struct fsmonitor_path, entry);
+
+	return strcmp(a->path, keydata ? keydata : b->path);
+}
+
+int fsmonitor_queue_path(struct fsmonitor_daemon_state *state,
+			 struct fsmonitor_queue_item **queue,
+			 const char *path, size_t len, uint64_t time)
+{
+	struct fsmonitor_path lookup, *e;
+	struct fsmonitor_queue_item *item;
+
+	hashmap_entry_init(&lookup.entry, len);
+	lookup.path = path;
+	lookup.len = len;
+	e = hashmap_get_entry(&state->paths, &lookup, entry, NULL);
+
+	if (!e) {
+		FLEXPTR_ALLOC_MEM(e, path, path, len);
+		e->len = len;
+		hashmap_put(&state->paths, &e->entry);
+	}
+
+	item = xmalloc(sizeof(*item));
+	item->path = e;
+	item->time = time;
+	item->previous = NULL;
+	item->next = *queue;
+	(*queue)->previous = item;
+	*queue = item;
+
+	return 0;
+}
+
+static int fsmonitor_run_daemon(void)
+{
+	pthread_t thread;
+	struct fsmonitor_daemon_state state = { { 0 } };
+	struct ipc_data ipc_data = {
+		.data = {
+			.path = git_path_fsmonitor(),
+			.handle_client = handle_client,
+		},
+		.state = &state,
+	};
+
+	hashmap_init(&state.paths, paths_cmp, NULL, 0);
+	pthread_mutex_init(&state.queue_update_lock, NULL);
+	pthread_mutex_init(&state.initial_mutex, NULL);
+	pthread_cond_init(&state.initial_cond, NULL);
+
+	pthread_mutex_lock(&state.initial_mutex);
+	if (pthread_create(&thread, NULL,
+			   (void *(*)(void *)) fsmonitor_listen, &state) < 0)
+		return error(_("could not start fsmonitor listener thread"));
+
+	/* wait for the thread to signal that it is ready */
+	while (!state.initialized)
+		pthread_cond_wait(&state.initial_cond, &state.initial_mutex);
+	pthread_mutex_unlock(&state.initial_mutex);
+
+	return ipc_listen_for_commands(&ipc_data.data);
+}
+#endif
 
 int cmd_fsmonitor__daemon(int argc, const char **argv, const char *prefix)
 {
@@ -34,7 +173,7 @@ int cmd_fsmonitor__daemon(int argc, const char **argv, const char *prefix)
 		usage_with_options(builtin_fsmonitor__daemon_usage, options);
 
 	if (mode == IS_SUPPORTED)
-		return 1;
+		return !FSMONITOR_DAEMON_IS_SUPPORTED;
 
-	die(_("no native fsmonitor daemon available"));
+	return !!fsmonitor_run_daemon();
 }
