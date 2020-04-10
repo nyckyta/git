6ba47e70a2eb8efe2116c12eb950ddb90c473d11
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-04-10 16:10:53 +0200

    fixup??? Reftable support for git-core
    The test for the reftable functionality should use the convenience
    functions we provide for test scripts. Using `test_commit` in particular
    does help with reproducible output (otherwise the SHA-1s will change
    together with the time the tests were run).
    
    Currently, this here seemingly innocuous change causes quite a few
    warnings throughout the test, though, e.g. this slur of warnings when
    committing the last commit in the test script:
    
    	warning: bad replace ref name: e
    	warning: bad replace ref name: ber-1
    	warning: bad replace ref name: ber-2
    	warning: bad replace ref name: ber-3
    	warning: bad replace ref name: ber-4
    	warning: bad replace ref name: ber-5
    	warning: bad replace ref name: ber-6
    	warning: bad replace ref name: ber-7
    	warning: bad replace ref name: ber-8
    	warning: bad replace ref name: ber-9
    
    This is notably _not_ a problem that was introduced by this here patch,
    of course, but a very real concern about the reftable patches, most
    likely the big one that introduces the reftable in one fell swoop.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t0031-reftable.sh
+++ t/t0031-reftable.sh
@@ -8,28 +8,26 @@ test_description='reftable basics'
 . ./test-lib.sh
 
 test_expect_success 'basic operation of reftable storage' '
-	git init --ref-storage=reftable repo && (
-	cd repo &&
-	echo "hello" >world.txt &&
-	git add world.txt &&
-	git commit -m "first post" &&
-	test_write_lines HEAD refs/heads/master >expect &&
+	rm -rf .git &&
+	git init --ref-storage=reftable &&
+	mv .git/hooks .git/hooks-disabled &&
+	test_commit file &&
+	test_write_lines HEAD refs/heads/master refs/tags/file >expect &&
 	git show-ref &&
 	git show-ref | cut -f2 -d" " > actual &&
 	test_cmp actual expect &&
 	for count in $(test_seq 1 10)
 	do
-		echo "hello" >>world.txt
-		git commit -m "number ${count}" world.txt ||
+		test_commit "number $count" file.t $count number-$count ||
 		return 1
 	done &&
 	git gc &&
-	nfiles=$(ls -1 .git/reftable | wc -l ) &&
-	test ${nfiles} = "2" &&
+	ls -1 .git/reftable >table-files &&
+	test_line_count = 2 table-files &&
 	git reflog refs/heads/master >output &&
 	test_line_count = 11 output &&
 	grep "commit (initial): first post" output &&
-	grep "commit: number 10" output )
+	grep "commit: number 10" output
 '
 
 test_done
